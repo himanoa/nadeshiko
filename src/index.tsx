8be6b1946332a766be13949330be118be48f3efd
himanoa <matsunoappy@gmail.com>
2018-02-24 22:41:03 +0900

    prettier

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,6 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 
-const rootElement = document.getElementById("root")
+const rootElement = document.getElementById("root");
 
-ReactDOM.render(
-  <h1>Poepoe</h1>, rootElement
-)
+ReactDOM.render(<h1>Poepoe</h1>, rootElement);
