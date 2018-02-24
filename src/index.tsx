30f44281a50992e994e6b5a20a73bef3b841c7bd
himanoa <matsunoappy@gmail.com>
2018-02-25 04:17:30 +0900

    Update

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import * as htmlparser from "htmlparser2";
-import { scheme } from "./models/index";
+import { scheme } from "./models";
 
 console.dir(scheme);
 const rootElement: HTMLElement | null = document.getElementById("root");
