40a01cc41be4bf751a1b6aa25fde0643abfc0e74
himanoa <matsunoappy@gmail.com>
2018-02-25 04:07:14 +0900

    Fix module solution

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,9 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import * as htmlparser from "htmlparser2";
-import * as scheme from "./models/index.ts";
+import * as fuga from "./models/index";
 
-console.table(scheme);
+console.table(fuga);
 const rootElement: HTMLElement | null = document.getElementById("root");
 
 ReactDOM.render(<h1>Poepoe</h1>, rootElement);
