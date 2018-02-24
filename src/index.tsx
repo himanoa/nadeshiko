478b6dadf792702ae5179a6310498677d8ada67b
himanoa <matsunoappy@gmail.com>
2018-02-25 03:11:00 +0900

    Add bookmark model

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import * as htmlparser from "htmlparser2";
+import Dexie from "dexie";
 
 const rootElement: HTMLElement | null = document.getElementById("root");
 
---
+++ src/models/bookmark.ts
@@ -0,0 +1,7 @@
+export interface Bookmark {
+  article_id: number;
+}
+
+export const scheme = {
+  rssFeeds: "article_id"
+};
