c6bd54dd1a0fcedfec302d5208f55b148dfa2d1b
himanoa <matsunoappy@gmail.com>
2018-02-25 16:01:39 +0900

    Add test case

--- src/repositories/articleRepository.test.js
+++ src/repositories/articleRepository.test.js
@@ -20,5 +20,21 @@ describe("ArticleRepository", () => {
         new ArticleRepository().whereByRssFeedId(0).toArray()
       ).resolves.toEqual([]);
     });
+    it("is result.length  == 1", async () => {
+      const ar = new ArticleRepository();
+      await ar.db.put({
+        rssFeedId: 0,
+        title: "title",
+        description: "de",
+        author: "a",
+        guid: "as",
+        pubdate: "asd",
+        linkUrl: "asd",
+        isAlreadyRead: true
+      });
+      expect(
+        (await new ArticleRepository().whereByRssFeedId(0).toArray()).length
+      ).toEqual(1);
+    });
   });
 });
