4c56cf62ddba174d637fb665c1ea910c8dad05b7
himanoa <matsunoappy@gmail.com>
2018-03-17 04:03:56 +0900

    Add rssImportWorker

--- src/workers/rssImportWorker.ts
+++ src/workers/rssImportWorker.ts
@@ -1,9 +1,14 @@
+import axios from "axios";
+import { YQLRssResponse } from "../types/yql";
 let timer;
+const url: string =
+  "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20rss%20where%20url%20%3D%20'https%3A%2F%2Fblog.himanoa.net%2Frss'&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys";
 onmessage = function(e) {
   if (e.data.payload != undefined) {
-    setTimeout(function() {
-      console.log("test");
-    }, 10);
-    console.dir("poepoe");
+    console.dir("poe");
+    timer = setInterval(async function() {
+      const page: YQLRssResponse = await axios.get(url);
+      console.dir(page);
+    }, 1000);
   }
 };
