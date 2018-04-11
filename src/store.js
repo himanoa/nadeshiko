f01eafd4e6b12d1ddda9a9838828dd8b9aec5e74
himanoa <matsunoappy@gmail.com>
2018-04-12 03:58:23 +0900

    Add dependency redux-logger

--- src/store.js
+++ src/store.js
@@ -1,3 +1,7 @@
-import * as rootReducer from "./reducers/index";
 import { createStore, applyMiddleware } from "redux";
+import logger from "redux-logger";
 
+import * as rootReducer from "./reducers/index";
+
+const store = createStore(rootReducer, applyMiddleware(logger));
+export default store;
