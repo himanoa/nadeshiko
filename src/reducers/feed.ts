69f4eca863dd44e4809bece185b86e8c407878ca
himanoa <matsunoappy@gmail.com>
2018-04-12 01:49:10 +0900

    Implement feed reducer

--- src/reducers/feed.ts
+++ src/reducers/feed.ts
@@ -1,5 +1,3 @@
-const actionCreator = actionCreatorFactory();
-
 export const ADD_FEED = "feed/addFeed";
 
 export type AddFeedPayload = {
@@ -8,15 +6,41 @@ export type AddFeedPayload = {
   updateInterval: number;
 };
 
-export default {
-  addFeed: (title: string, feedUrl: string, updateInterval: number) => {
-    return {
-      type: ADD_FEED,
-      payload: {
-        title,
-        feedUrl,
-        updateInterval
-      }
-    };
+export type Action<T> = {
+  type: string;
+  payload: T;
+};
+
+export const addFeed = (
+  title: string,
+  feedUrl: string,
+  updateInterval: number
+): Action<AddFeedPayload> => {
+  return {
+    type: ADD_FEED,
+    payload: {
+      title,
+      feedUrl,
+      updateInterval
+    }
+  };
+};
+
+export type State = {
+  feeds: AddFeedPayload[];
+};
+
+export const initialState: State = { feeds: [] };
+
+export default (state: State = initialState, action: Action): State => {
+  switch (action.type) {
+    case ADD_FEED: {
+      return {
+        feeds: [...state.feeds, action.payload]
+      };
+    }
+    default: {
+      return state;
+    }
   }
 };
