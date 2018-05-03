50dd0a43702a3c6c4963d6a43e4d1a4a301038b7
himanoa <matsunoappy@gmail.com>
2018-05-03 16:27:47 +0900

    Store

--- src/store.ts
+++ src/store.ts
@@ -1,7 +1,11 @@
 import { createStore, applyMiddleware } from "redux";
 import logger from "redux-logger";
+import createSagaMiddleware from "redux-saga";
 
 import { default as rootReducer } from "./reducers/index";
 
-const store = createStore(rootReducer, applyMiddleware(logger));
+const sagaMiddleware = createSagaMiddleware();
+const store = createStore(rootReducer, applyMiddleware(logger, sagaMiddleware));
+
+// sagaMiddleware.run(mySaga)
 export default store;
