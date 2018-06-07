fc9202fa6b3e30b3ac353110c43f8833eb3dae39
himanoa <matsunoappy@gmail.com>
2018-06-08 03:31:35 +0900

    Fix tslint

--- src/repositories/articleRepository.ts
+++ src/repositories/articleRepository.ts
@@ -4,11 +4,11 @@ import { NadeshikoDatabase } from "../db";
 import { Article } from "../models/article";
 
 export class ArticleRepository {
-  private db: Dexie.Table<Article, number> = NadeshikoDatabase.instance
-    .articles;
   public importFeed: (feedId: number, articles: Article[]) => Promise<boolean>;
   public whereByRssFeedIdCount: (feedId: number) => Promise<number>;
   public asyncWhereByRssFeedId: (feedId: number) => Promise<Article[]>;
+  private db: Dexie.Table<Article, number> = NadeshikoDatabase.instance
+    .articles;
   constructor() {
     this.importFeed = async (
       feedId: number,
